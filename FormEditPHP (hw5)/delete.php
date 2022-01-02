f9bb6961ad521268645c0ee24623420f41d8b4e1
Aiden Smith <29802327+Voxel20@users.noreply.github.com>
2022-01-01 20:10:55 -0500

    Delete FormEditPHP (hw5) directory

--- FormEditPHP (hw5)/delete.php
+++
@@ -1,4 +0,0 @@
-<?php require_once 'include/functions.php';
-
-deleteDogname($_GET['id'] ?? 0);
-header('Location: ./');
--- FormEditPHP (hw5)/submitedit.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'include/functions.php';
-
-handleSubmitEdit();
